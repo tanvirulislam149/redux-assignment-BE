4e32d9dbab3aebe0654024194c39e54b3373ac23
tanvir <tanvirulislam149@gmail.com>
2024-01-27 01:25:38 +0600

    delete done

--- src/app/modules/items/items.controller.js
+++ src/app/modules/items/items.controller.js
@@ -13,8 +13,16 @@ exports.getAllItems = async (req, res, next) => {
 exports.AddNewItem = async (req, res, next) => {
   try {
     const data = req.body;
-    console.log(data);
     const result = await ItemsModel.create(data);
+    res.send(result);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+}
+exports.DeleteItem = async (req, res, next) => {
+  try {
+    const data = req.body;
+    const result = await ItemsModel.deleteOne(data);
     res.send(result);
   } catch (error) {
     res.status(500).send({ message: error.message });
--- src/app/modules/items/items.route.js
+++ src/app/modules/items/items.route.js
@@ -5,5 +5,6 @@ const itemsController = require("./items.controller");
 
 router.get("/getAllItems", itemsController.getAllItems);
 router.post("/addItem", itemsController.AddNewItem);
+router.delete("/deleteItem", itemsController.DeleteItem);
 
 module.exports = router;

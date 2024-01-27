9ad85c28eca3773ef62cffe0cd6cc7d8addb6824
tanvir <tanvirulislam149@gmail.com>
2024-01-27 14:32:21 +0600

    update done

--- src/app/modules/items/items.controller.js
+++ src/app/modules/items/items.controller.js
@@ -9,6 +9,14 @@ exports.getAllItems = async (req, res, next) => {
     res.status(500).send({ message: error.message });
   }
 }
+exports.getOneItem = async (req, res, next) => {
+  try {
+    const result = await ItemsModel.findOne(req.params).exec();
+    res.send({ result });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+}
 
 exports.AddNewItem = async (req, res, next) => {
   try {
@@ -23,6 +31,16 @@ exports.DeleteItem = async (req, res, next) => {
   try {
     const data = req.body;
     const result = await ItemsModel.deleteOne(data);
+    res.send(result);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+}
+exports.UpdateItem = async (req, res, next) => {
+  try {
+    const { _id, name, brand, category, condition, compatibility, color, capacity, form_factor, interface, price, quantity } = req.body;
+    const update = { name, brand, category, condition, compatibility, color, capacity, form_factor, interface, price, quantity };
+    const result = await ItemsModel.findOneAndUpdate({ _id }, update);
     res.send(result);
   } catch (error) {
     res.status(500).send({ message: error.message });
--- src/app/modules/items/items.route.js
+++ src/app/modules/items/items.route.js
@@ -6,5 +6,7 @@ const itemsController = require("./items.controller");
 router.get("/getAllItems", itemsController.getAllItems);
 router.post("/addItem", itemsController.AddNewItem);
 router.delete("/deleteItem", itemsController.DeleteItem);
+router.put("/updateItem", itemsController.UpdateItem);
+router.get("/getOneItem/:_id", itemsController.getOneItem);
 
 module.exports = router;

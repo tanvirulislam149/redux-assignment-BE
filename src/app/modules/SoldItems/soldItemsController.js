7771a2124a4c77861fe6476e18d789c4702d84f0
tanvir <tanvirulislam149@gmail.com>
2024-01-27 19:45:20 +0600

    sell item done

--- src/app/modules/SoldItems/soldItemsController.js
+++ src/app/modules/SoldItems/soldItemsController.js
@@ -5,9 +5,13 @@ exports.postSoldItem = async (req, res, next) => {
   try {
     const find = await ItemsModel.findOne({ _id: req.body.id }).exec();
     const result = await SoldItemsModel.create(req.body);
-    const update = await ItemsModel.findOneAndUpdate({ _id: req.body.id }, {
-      $set: { quantity: find.quantity - req.body.quantity }
-    });
+    if ((find.quantity - req.body.quantity) > 0) {
+      const update = await ItemsModel.findOneAndUpdate({ _id: req.body.id }, {
+        $set: { quantity: find.quantity - req.body.quantity }
+      });
+    } else {
+      const deleteItem = await ItemsModel.deleteOne({ _id: req.body.id });
+    }
     res.send(result);
   } catch (error) {
     res.status(500).send({ message: error.message });

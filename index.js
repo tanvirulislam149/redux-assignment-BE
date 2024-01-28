d1b432ce8c052a48e3ca68e316116b1073c9e4dd
tanvir <tanvirulislam149@gmail.com>
2024-01-29 00:49:47 +0600

    cors check

--- index.js
+++ index.js
@@ -6,17 +6,32 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 var jwt = require('jsonwebtoken');
 const itemsRoute = require("./src/app/modules/items/items.route")
-const soldItemsRoute = require("./src/app/modules/SoldItems/SoldItemsRoute");
-const verifyJWT = require('./src/utils/verifyJWT');
+const soldItemsRoute = require("./src/app/modules/SoldItems/SoldItemsRoute")
 
 
 // middleware
 app.use(express.json());
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "DELETE", "UPDATE", "PUT"],
-  credentials: true,
-}))
+app.use(cors())
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "http://localhost:5173/"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
 
 async function main() {
   const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ejazoj4.mongodb.net/redux-assignment?retryWrites=true&w=majority`;
@@ -29,10 +44,6 @@ main();
 // Routes
 app.use("/items", itemsRoute)
 app.use("/soldItems", soldItemsRoute)
-app.use("/soldItems/getAllItems", verifyJWT, async (req, res) => {
-  const result = await ItemsModel.find({});
-  res.send({ result });
-})
 
 // jwt token route
 app.post("/getToken", async (req, res) => {
